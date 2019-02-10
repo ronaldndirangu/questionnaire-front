f926a9b10eb67b8286a17af6a9be380e6d528635
Ronald Ndirangu <ronald.ndirangu@gmail.com>
2019-02-10 11:59:19 +0300

    Add view meetup feature

--- questionnaire-front/src/views/ViewMeetup.vue
+++ questionnaire-front/src/views/ViewMeetup.vue
@@ -3,14 +3,23 @@
     <Banner />
     <div class="meetup-details">
       <div class="meetup-header">
-        <div class="date">
-          {{ formatDate }}
+        <div class="left">
+          <div class="date">
+            {{ formatDate(getMeetup[0].happeningOn) }}
+          </div>
+          <div class="topic" v-if="getMeetup[0]">
+            {{ getMeetup[0].topic }}
+          </div>
         </div>
-        <div class="topic" v-if="getMeetup">
-          {{ getMeetup[0].topic }}
+          <div class="right">
+            <p>Are you going?</p>
+            <div class="btns">
+              <button type="submit" class="rsvp-btn">Yes</button>
+              <button type="submit" class="rsvp-btn">No</button>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
   </div>
 </template>
 
@@ -28,13 +37,15 @@ export default {
     ...mapGetters([
       'getMeetup',
     ]),
-    formatDate: () => {
-      return moment().format('YYYY MM DD')
-    }
   },
   created() {
     const meetupId = this.$route.params.meetupId;
     this.$store.dispatch('getMeetup', meetupId)
+  },
+  methods: {
+    formatDate: (date) => {
+      return moment(date).format('LLLL')
+    }
   }
 }
 </script>
@@ -43,16 +54,61 @@ export default {
   .meetup-details {
     display: flex;
     flex-direction: column;
+    position: relative;
     align-content: center;
     background: #fff;
     width: 70%;
-    margin: 10% 15%;
+    margin: 8% 15%;
     z-index: 5;
     position: absolute;
     top: 0;
   } 
   .meetup-header {
-   padding: 2% 2%;
+    padding: 2% 2%;
+    display: flex;
+    flex-direction: row;
+    border-bottom: solid 1px #ccc;
+  }
+  .date {
+    color: rgba(46,62,72,.6);
+    margin-bottom: 3px;
+  }
+  .topic {
+    font-size: 32px;
+    font-weight: 0.8px;
+    width: 80%;
+  }
+  .rsvp {
+    display: flex;
+    width: 20%;
+    flex-direction: column;
+  }
+  .left {
+    display: flex;
+    width: 80%;
+    flex-direction: column;
+  }
+  .right {
+    display: flex;
+    width: 20%;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+  .btns {
+    display: flex;
+    flex-direction: row;
+  }
+  .rsvp-btn {
+    padding: 10px 30px;
+    margin: auto 2px;
+    background: #2359af;
+    color: #fff;
+    border-radius: 5px;
+    font-size: 0.8em;
+  }
+  .rsvp-btn:focus {
+    outline: none;
   }
 </style>
 
